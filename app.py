c51bf29b4e7639e87b3d62f2a8e816841301aaf6
Momin-Mohammed-Saad <saadsiddiqui7103@gmail.com>
2026-07-15 21:03:35 +0530

    Add Streamlit user input interface

--- app.py
+++ app.py
@@ -16,3 +16,19 @@ vectorizer = joblib.load("models/tfidf_vectorizer.joblib")
 st.title("🎬 Movie Genre Classification")
 
 st.write("Predict the genre of a movie using its plot summary.")
+
+# user input
+movie_description =  st.text_area(
+    "Enter Movie Description",
+    height = 250,
+    placeholder="Example: A young boy discovers he has magical powers and attends a school of wizardry..."
+)
+
+# prediction Button
+predict_button = st.button("Predict Genre")
+
+if predict_button:
+    st.write("Input Received ✅")
+    st.write(movie_description)
+
+    
